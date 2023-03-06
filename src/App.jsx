85e4ee2424a87cebb1d199381c36b0e970d13b8c
Rafael Guilber <Rafaelguilberdosreis@hotmail.com>
2023-03-06 12:23:27 +0000

    :sparkles: feat: second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Header } from "./components/Header";
 
+import style from "./App.module.css";
+
 function App() {
   return (
     <>
       <Header />
-      <h1>ola, isto é um inicio</h1>
+      <div className={style.wrapper}>
+        <aside>aside</aside>
+        <main>main</main>
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import igniteLogo from "../assets/ignite-logo.svg";
+
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Ignite Lab</h1>
+      <img src={igniteLogo} alt="Logotipo do ignite rocketseat" />
     </header>
   );
 };
