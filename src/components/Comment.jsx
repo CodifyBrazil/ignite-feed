89065653416c24ba96793d49b807a74dfa4cbc7e
Rafael Guilber <Rafaelguilberdosreis@hotmail.com>
2023-03-07 20:03:03 +0000

    :sparkles: feat: created components comments

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import style from "./Comment.module.css";
+
+export const Comment = ({ content, onDeleteComment }) => {
+  const handleDeleteComment = () => {
+    onDeleteComment(content);
+  };
+
+  return (
+    <div className={style.comment}>
+      <Avatar hasBorder={false} src="https://github.com/codifybrazil.png" />
+
+      <div className={style.commentBox}>
+        <div className={style.commentContent}>
+          <header>
+            <div className={style.authorAndTime}>
+              <strong>Rafael Guilber</strong>
+              <time title="11 de Maio ás 08:13" dateTime="2022-05-11 08:13">
+                Cerca de 1h átras
+              </time>
+            </div>
+            <button title="Deletar comentário" onClick={handleDeleteComment}>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
