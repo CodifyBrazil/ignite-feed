03919351204b5873cc27cfff6f29f0ca24258ac6
Rafael Guilber <Rafaelguilberdosreis@hotmail.com>
2023-03-07 20:02:41 +0000

    :sparkles: feat: created avatar components

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,10 @@
+import style from "./Avatar.module.css";
+
+export const Avatar = ({ hasBorder = true, src }) => {
+  return (
+    <img
+      className={hasBorder ? style.avatarWithBorder : style.avatar}
+      src={src}
+    />
+  );
+};
